3ee8ae395df9410e874c647415511c4ea4790e2a
Davey Grotendorst (122693) <dave-grotendorst@hotmail.com>
2015-06-12 18:06:01 +0200

    Changed abdomen to be larger in spawn-state

--- main/java/diversity/client/model/ModelDarkSpider.java
+++ main/java/diversity/client/model/ModelDarkSpider.java
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import diversity.entity.EntityDarkSpider;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.model.ModelSpider;
 import net.minecraft.entity.Entity;
@@ -22,6 +23,7 @@ public class ModelDarkSpider extends ModelSpider
     public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_)
     {
         this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_, p_78088_1_);
+        EntityDarkSpider spider = (EntityDarkSpider)p_78088_1_;
         this.spiderHead.render(p_78088_7_);
         this.spiderNeck.render(p_78088_7_);
         this.spiderLeg1.render(p_78088_7_);
@@ -32,15 +34,24 @@ public class ModelDarkSpider extends ModelSpider
         this.spiderLeg6.render(p_78088_7_);
         this.spiderLeg7.render(p_78088_7_);
         this.spiderLeg8.render(p_78088_7_);
-        GL11.glPushMatrix();
-        GL11.glScalef((scale/2) * 2.5f, 1f, 1f);
+        
+        if(spider.getPhase() < 2)
+        {
+        	GL11.glPushMatrix();
+        	GL11.glScalef((scale/2) * 2.0f, 2f, 2f);
+        	GL11.glTranslatef(0f, -0.45f, -0.1f);
         	this.spiderBody.render(p_78088_7_);
-        GL11.glPopMatrix();
+        	GL11.glPopMatrix();
+        }
+        else
+        {
+        	this.spiderBody.render(p_78088_7_);
+        }
     }
 	
 	public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity p_78087_7_)
     {
 		super.setRotationAngles(p_78087_1_, p_78087_2_, p_78087_3_, p_78087_4_, p_78087_5_, p_78087_6_, p_78087_7_);
-		scale = p_78087_3_;
+		scale = p_78087_3_ + 2;
     }
 }
